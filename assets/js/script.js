be176ecb08e5aa87ac5e326d44b16b05c5a91bcb
gustavomena <gustavo.mena@gmail.com>
2021-10-23 00:22:59 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// cargando el texto de los parrafos por js para que sea mas limpio el codigo html
+
+const whoWeAre_Card1="Mots en un emprendimiento familiar dedicado a la comercialización e implementación de equipamientos,maquinaria, servicios y mantenimiento para atender los procesos de las industrias automotriz, minería, forestal e industrial.";
+
+document.getElementById("txtCard1").innerHTML = whoWeAre_Card1;
+
+const whoWeAre_Card2="Ingresa al mercado automotriz, minero e industrial con el fin de entregar su experiencia en equipamiento hacia talleres y concesionarios de mantenimiento como carrocería, empresas mineras, industriales,forestales y de energías renovables.";
+
+document.getElementById("txtCard2").innerHTML = whoWeAre_Card2;
+
+const whoWeAre_Card3="Hoy proponemos en nuestra oferta técnica especializada soluciones integrales y de calidad a nuestros clientes de todos los rubros que utilizan diversos tipos de herramientas en sus procesos productivos y de servicio, manteniendo una mejora continua a través de asesoría técnica y post venta.";
+
+document.getElementById("txtCard3").innerHTML = whoWeAre_Card3;
